1d3bedc378b6e29f1012e945987db7973978427b
Abdul Moiz <moiz05366@gmail.com>
2024-12-07 15:11:48 +0500

    add animation

--- app.js
+++ app.js
@@ -37,75 +37,163 @@ accordionItems.forEach((item) => {
         }
     });
 });
+// ------------
 
 // GSAP ANIMATION
-
 // header item animation
-const tl = gsap.timeline();
-tl.from("nav a, header button,  .logo", {
-    opacity: 0,
-    y: -40,
-    stagger: 0.2,
-    duration: .5,
-    delay: 0.5
-});
+const headerAnim = () => {
+    const tl = gsap.timeline();
+    tl.from("nav a, header button,  .logo", {
+        opacity: 0,
+        y: -40,
+        stagger: 0.2,
+        duration: .3,
+        delay: 0.5
+    });
 
-tl.from(".hero-para, .hero-head, .hero-btn", {
-    x: -100,
-    opacity: 0,
-    duration: .4,
-    stagger: 0.2,
-}, "-=.5");
+    tl.from(".hero-para, .hero-head, .hero-btn", {
+        x: -100,
+        opacity: 0,
+        duration: .3,
+        stagger: 0.2,
+    }, "-=.5");
 
-tl.from(".hero-img", {
-    x: 100,
-    opacity: 0,
-    duration: .5,
-    stagger: 0.2,
-});
-
+    tl.from(".hero-img", {
+        x: 100,
+        opacity: 0,
+        duration: .3,
+        stagger: 0.2,
+    });
+}
 
 // branding images animations
 
-gsap.from(".branding-wrapper>div", {
-    opacity: 0,
-    duration: 1,
-    y: 20,
-    stagger: 0.2,
-    ease: "power3.out",
-    scrollTrigger: {
-        trigger: ".branding-wrapper>div",  // Element to trigger the animation
-        scroller: "body",
-        start: "top 80%",  // When the top of the box reaches 80% of the viewport
-        end: "top 30%",    // When the top of the box reaches 30% of the viewport
-        // markers: true      // Shows start and end markers (for debugging)
-    }
-});
+const brandingImagesAnim = () => {
+    gsap.from(".branding-wrapper>div", {
+        opacity: 0,
+        duration: 1,
+        y: 20,
+        stagger: 0.2,
+        ease: "power3.out",
+        scrollTrigger: {
+            trigger: ".branding-wrapper>div",  // Element to trigger the animation
+            scroller: "body",
+            start: "top 80%",  // When the top of the box reaches 80% of the viewport
+            end: "top 30%",    // When the top of the box reaches 30% of the viewport
+        }
+    });
+}
 
 // services animation 
-const tl2 = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".services",  // Element to trigger the animation
-        scroller: "body",
-        start: "top 20%",  // When the top of the box reaches 80% of the viewport
-        end: "top 30%",    // When the top of the box reaches 30% of the viewport
-        markers: true,   // Shows start and end markers (for debugging)
-        stagger: .4,
-    }
-});
-tl2.from(".services :is(.primary-para, .primary-heading)", {
-    opacity: 0,
-    duration: 1,
-    x: -100,
-});
+const servicesAnim = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".services",  // Element to trigger the animation
+            scroller: "body",
+            start: "top 50%",  // When the top of the box reaches 80% of the viewport
+            end: "top -50%",    // When the top of the box reaches 30% of the viewport
+            stagger: .4,
+            scrub: 2,
+        }
+    });
+    tl.from(".services :is(.primary-para, .primary-heading)", {
+        opacity: 0,
+        duration: 1,
+        x: -100,
+    });
 
-tl2.from(".services-row >div:first-child", {
-    opacity: 0,
-    duration: 1,
-    x: -100,
-});
-tl2.from(".services-row >div:nth-child(2)", {
-    opacity: 0,
-    duration: 1,
-    x:100
-});
+    tl.from(".services-row >div:first-child", {
+        opacity: 0,
+        duration: 1,
+        x: -100,
+    }, "anim");
+    tl.from(".services-row >div:nth-child(2)", {
+        opacity: 0,
+        duration: 1,
+        x: 100
+    }, "anim");
+    tl.from(".services-row >div:nth-child(3)", {
+        opacity: 0,
+        duration: 1,
+        x: -100,
+    }, "anim2");
+    tl.from(".services-row >div:nth-child(4)", {
+        opacity: 0,
+        duration: 1,
+        x: 100
+    }, "anim2");
+}
+
+
+// working process section animation
+const workingProcessAnim = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".process",  // Element to trigger the animation
+            scroller: "body",
+            start: "top 50%",  // When the top of the box reaches 80% of the viewport
+            end: "top -20%",    // When the top of the box reaches 30% of the viewport
+            stagger: .4,
+            scrub: 2,
+        }
+    });
+    tl.from(".process .primary-heading", {
+        opacity: 0,
+        duration: .5,
+        y: -40,
+    });
+
+
+    tl.from(".process p.primary-para", {
+        opacity: 0,
+        duration: .4,
+        y: 40,
+    })
+
+    // accordian animations
+    tl.from(".my-accordion-item.left", {
+        opacity: 0,
+        duration: .4,
+        x: -100,
+        stagger: .5
+    }, "anim1")
+    tl.from(".my-accordion-item.right", {
+        opacity: 0,
+        duration: .4,
+        x: 100,
+        stagger: .4
+    }, "anim1")
+}
+
+// contact form animation 
+
+const contactFormAnim = () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".contact-us",
+            scroller: "body",
+            start: "top 50%",
+            end: "top -20%",
+            scrub: true
+        }
+    });
+    tl.from(".form-container", {
+        opacity: 0,
+        duration: .4,
+        y: -200
+    });
+}
+const serviceLinksArrowAnim = () => {
+    gsap.to(".service-links-arrow-icon", {
+        x: 200,
+        duration: 1,
+        yoyo: true,
+        repeat: -1,
+    });
+}
+headerAnim();
+brandingImagesAnim();
+servicesAnim();
+workingProcessAnim();
+contactFormAnim();
+serviceLinksArrowAnim();
